fe230e5b4c008ff711730370a20541f929a89bce
F-G Fernandez <fgfm03@hotmail.fr>
2020-02-23 17:45:45 +0100

    fix: Fixed dropout on LeNet5 (#5)
    By default dropout is disabled. However, if it is used, the output layer should not have any, only the hidden layers.

--- src/models.py
+++ src/models.py
@@ -27,15 +27,15 @@ def lenet5(dropout_prob=0.2):
         MaxPooling2D((2, 2)),
         Conv2D(16, (5, 5), activation='relu'),
         MaxPooling2D((2, 2)),
-        Flatten(),
-        Dense(120, activation='relu')
+        Flatten()
     ]
 
     if dropout_prob > 0:
         layers.append(Dropout(dropout_prob))
-    layers.append(Dense(84, activation='relu'))
+    layers.append(Dense(120, activation='relu'))
     if dropout_prob > 0:
         layers.append(Dropout(dropout_prob))
+    layers.append(Dense(84, activation='relu'))
     layers.append(Dense(43, activation='softmax'))
 
     return Sequential(layers)
